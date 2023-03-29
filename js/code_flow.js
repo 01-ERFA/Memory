06e781252f8776e83f11154bbec25cb809195095
01-ERFA <atukdibe@gmail.com>
2023-03-29 18:55:19 -0300

    add component 'home'

--- js/code_flow.js
+++ js/code_flow.js
@@ -1,16 +1,19 @@
 import { generate_app, modify_html } from "./modules/html.js"
 import header  from "../js/components/header.js"
+import home from "./components/home.js"
 
 export function layaout(language) {
 
     const app = new modify_html(generate_app())
 
     app.add_html(`
-        <header id="header">
+        <header>
             ${header(language)}
         </header>
-        <main id="main"></main>
-        <footer id="footer"></footer>
+        <main id="main">
+            ${home(language)}
+        </main>
+        <footer></footer>
     `)
 
     // const main = new modify_html(document.getElementById('main'))
---
+++ js/components/home.js
@@ -0,0 +1,20 @@
+import { languages } from "../data/web.js";
+
+const home = (language)=>{
+    return `
+
+        <div class='home'>
+            <h1 class='title'>
+                i am a title of home
+            </h1>
+
+
+
+
+
+        </div>
+    
+    `
+}
+
+export default home
